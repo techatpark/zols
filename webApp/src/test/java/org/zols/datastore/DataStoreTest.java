ca8f6976ab3c62ca62976a7fe2ef8d7d252c3680
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2013-09-24 20:20:05 +0530

    Super Use Case working

--- webApp/src/test/java/org/zols/datastore/DataStoreTest.java
+++ webApp/src/test/java/org/zols/datastore/DataStoreTest.java
@@ -1,6 +1,7 @@
 package org.zols.datastore;
 
 import java.beans.IntrospectionException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,60 +11,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zols.datastore.model.Attribute;
-import org.zols.datastore.model.Schema;
+import org.zols.datastore.model.BaseObject;
+import org.zols.datastore.model.Entity;
 import org.zols.datastore.util.DynamicBeanGenerator;
 import org.zols.test.loader.AnnotationConfigContextLoader;
 
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class, value = "org.zols.core.config.SpringMongoConfig")
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, value = {"org.zols.core.web.config.ControllerConfiguration"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DataStoreTest {
 
-	@Autowired
-	private DataStore dataStore;
+    @Autowired
+    private DataStore dataStore;
+    
+    @Autowired
+    private DynamicBeanGenerator beanGenerator;
 
-	
+    @Test
+    public void testentityCreate() {
 
-	public void testSchemaCreate() {
-            
-            for (int i = 0; i < 10; i++) {
-                Schema schema = new Schema();
-		schema.setName(String.valueOf(i));
-		schema.setLabel("Employee");
-		schema.setDescription("Describe an Employee of the Organization");
-		
-		Attribute attribute = null ;
-		
-		List<Attribute> attributes = new ArrayList<Attribute>(2);
-		
-		attribute = new Attribute() ;
-		attribute.setDescription("Name of the Employee");
-		attribute.setName("name");
-		attribute.setType("String");
-		attributes.add(attribute);
-		
-		attribute = new Attribute() ;
-		attribute.setDescription("Age of the Employee");
-		attribute.setName("age");
-		attribute.setType("Integer");
-		attributes.add(attribute);
-		
-		schema.setAttributes(attributes);
-		
-		dataStore.create(schema,Schema.class);
-		System.out.println("dataStore " + schema.getLabel());
-            }
-            
-		
-	}
-	
-        @Test
-	public void testMethod() {
-            
+        Entity entity = dataStore.read("basic", Entity.class);
+        if (entity == null) {
+            entity = new Entity();
+            entity.setName("basic");
+            entity.setLabel("Basic Entity");
+            entity.setDescription("Describe an Basic Entity");
+
+            Attribute attribute = null;
+
+            List<Attribute> attributes = new ArrayList<Attribute>(1);
+
+            attribute = new Attribute();
+            attribute.setDescription("Count is an Integer");
+            attribute.setName("count");
+            attribute.setType("Integer");
+            attributes.add(attribute);
+
+            entity.setAttributes(attributes);
+            dataStore.create(entity, Entity.class);
         }
-	public void testCreateBean() throws IntrospectionException, InstantiationException, IllegalAccessException {
-		Schema schema = dataStore.read("employee", Schema.class) ;
-		Object object = DynamicBeanGenerator.generate(schema);
-		System.out.println(object);
-	}
+    }
 
+    @Test
+    public void testCreateBean() throws IntrospectionException, InstantiationException, IllegalAccessException {
+        final Class<?> beanClass =
+                beanGenerator.getBeanClass("basic");
+        System.out.println(beanClass);
+        for (final Method method : beanClass.getMethods()) {
+            System.out.println(method);
+        }
+    }
+    
+    @Test
+    public void testCreateDynamicObjectFromJSON() {
+        String jsonString = "{ \"name\": \"new\",\"count\": 2 }";        
+        BaseObject baseObject = beanGenerator.getBaseObject("basic", jsonString);
+        System.out.println(baseObject.getName());
+    }
 }
