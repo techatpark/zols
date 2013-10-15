f27da55276177df196fb744c72cd398021c540d0
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2013-10-15 13:14:09 +0530

    Removed Ideal Form. Let us get basics working

--- webApp/src/main/java/org/zols/web/config/ViewConfiguration.java
+++ webApp/src/main/java/org/zols/web/config/ViewConfiguration.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring3.SpringTemplateEngine;
 import org.thymeleaf.spring3.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 public class ViewConfiguration {
-
+    
     public ClassLoaderTemplateResolver classLoaderTemplateResolver() {
         ClassLoaderTemplateResolver classLoaderTemplateResolver = new ClassLoaderTemplateResolver();
         classLoaderTemplateResolver.setSuffix(".html");
@@ -17,14 +18,25 @@ public class ViewConfiguration {
         classLoaderTemplateResolver.setCacheable(false);
         return classLoaderTemplateResolver;
     }
-
+    
+    public ServletContextTemplateResolver servletContextTemplateResolver() {
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+        resolver.setPrefix("/WEB-INF/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(1);
+        resolver.setCacheable(false);
+        return resolver;
+    }
+    
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addTemplateResolver(servletContextTemplateResolver());
         engine.addTemplateResolver(classLoaderTemplateResolver());        
         return engine;
     }
-
+    
     @Bean
     public ThymeleafViewResolver thymeleafViewResolver() {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
