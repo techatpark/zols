86a9c34ad29066ff9934f5635ae536ddd5a74a7e
Poomalai <poomalaisv@gmail.com>
2013-11-06 17:43:05 +0530

    Superb Work  by Navin and Poomalai

--- webApp/src/main/java/org/zols/datastore/web/controller/EntityController.java
+++ webApp/src/main/java/org/zols/datastore/web/controller/EntityController.java
@@ -2,6 +2,8 @@ package org.zols.datastore.web.controller;
 
 import com.zols.datastore.DataStore;
 import com.zols.datastore.domain.Entity;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,81 +24,82 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping(value = "/entities")
 public class EntityController {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(EntityController.class);
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(EntityController.class);
 
-	@Autowired
-	private DataStore dataStore;
+    @Autowired
+    private DataStore dataStore;
 
-	@RequestMapping(method = POST)
-	@ResponseBody
-	public Entity create(@RequestBody Entity entity) {
-		LOGGER.info("Creating new entity {}", entity);
-		return dataStore.create(entity, Entity.class);
-	}
+    @RequestMapping(method = POST)
+    @ResponseBody
+    public Entity create(@RequestBody Entity entity) {
+        LOGGER.info("Creating new entity {}", entity);
+        return dataStore.create(entity, Entity.class);
+    }
 
-	@RequestMapping(value = "/{name}", method = GET)
-	@ResponseBody
-	public Entity read(@PathVariable(value = "name") String name) {
-		LOGGER.info("Reading entity with id {}", name);
-		Entity entity = dataStore.read(name, Entity.class);		
-		return entity;
-	}
+    @RequestMapping(value = "/{name}", method = GET)
+    @ResponseBody
+    public Map<String, Entity> read(@PathVariable(value = "name") String name) {
+        LOGGER.info("Reading entity with id {}", name);
+        Map<String, Entity> map = new HashMap<String, Entity>(1);
+        map.put("entity", dataStore.read(name, Entity.class));
+        return map;
+    }
 
-	@RequestMapping(value = "/{name}", method = PUT)
-	@ResponseStatus(value = HttpStatus.NO_CONTENT)
-	public void update(@PathVariable(value = "name") String name,
-			@RequestBody Entity entity) {
-		LOGGER.info("Updating entity with id {} with {}", name, entity);
-		dataStore.update(entity, Entity.class);
-	}
+    @RequestMapping(value = "/{name}", method = PUT)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@PathVariable(value = "name") String name,
+            @RequestBody Entity entity) {
+        LOGGER.info("Updating entity with id {} with {}", name, entity);
+        dataStore.update(entity, Entity.class);
+    }
 
-	@RequestMapping(value = "/{name}", method = DELETE)
-	@ResponseStatus(value = HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable(value = "name") String name) {
-		LOGGER.info("Deleting entity with id {}", name);
-		dataStore.delete(name, Entity.class);
-	}
+    @RequestMapping(value = "/{name}", method = DELETE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable(value = "name") String name) {
+        LOGGER.info("Deleting entity with id {}", name);
+        dataStore.delete(name, Entity.class);
+    }
 
-	@RequestMapping(method = GET)
-	@ResponseBody
-	public Page<Entity> list(
-			Pageable page) {
-		LOGGER.info("Listing entities");
-		return dataStore.list(page, Entity.class);
-	}
+    @RequestMapping(method = GET)
+    @ResponseBody
+    public Page<Entity> list(
+            Pageable page) {
+        LOGGER.info("Listing entities");
+        return dataStore.list(page, Entity.class);
+    }
 
-	@ExceptionHandler(IllegalArgumentException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ResponseBody
-	public String handleClientErrors(Exception ex) {
-		LOGGER.error(ex.getMessage(), ex);
-		return ex.getMessage();
-	}
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleClientErrors(Exception ex) {
+        LOGGER.error(ex.getMessage(), ex);
+        return ex.getMessage();
+    }
 
-	@ExceptionHandler(Exception.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	@ResponseBody
-	public String handleServerErrors(Exception ex) {
-		LOGGER.error(ex.getMessage(), ex);
-		return ex.getMessage();
-	}
-        
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public String handleServerErrors(Exception ex) {
+        LOGGER.error(ex.getMessage(), ex);
+        return ex.getMessage();
+    }
+
     @RequestMapping(value = "/edit/{name}", method = GET)
-    public String edit(@PathVariable(value = "name") String name,Model model) { 
-        model.addAttribute("entity", dataStore.read(name, Entity.class)); 
+    public String edit(@PathVariable(value = "name") String name, Model model) {
+        model.addAttribute("entity", dataStore.read(name, Entity.class));
         return "datastore/entity";
     }
-    
+
     @RequestMapping(value = "/add", method = GET)
-    public String add(Model model) {   
-        model.addAttribute("entity",new Entity());
+    public String add(Model model) {
+        model.addAttribute("entity", new Entity());
         return "datastore/entity";
     }
-    
+
     @RequestMapping(value = "/listing", method = GET)
-    public String listing() {         
+    public String listing() {
         return "datastore/listentities";
     }
-    
+
 }
