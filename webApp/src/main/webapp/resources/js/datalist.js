5c0514cb0e5afca015c244a88f2fdfd8912af795
navin_kr <navin_kr@VIJAYANAND_P>
2013-11-20 15:12:40 +0530

    Delete function fixed..

--- webApp/src/main/webapp/resources/js/datalist.js
+++ webApp/src/main/webapp/resources/js/datalist.js
@@ -1,103 +1,103 @@
 var URL = '../api/';
 
-function loadData(entityName, contentData) {    
-        $.extend($.jgrid.defaults, {
-            autowidth: true,
-            shrinkToFit: true,
-            datatype: 'json',
-            jsonReader: {
-                repeatitems: false,
-                root: "content",
-                page: function(result) {
-                    //Total number of records
-                    return result.number + 1;
-                },
-                total: "totalPages",
-                records: "totalElements"
+function loadData(entityName, contentData) {
+    $.extend($.jgrid.defaults, {
+        autowidth: true,
+        shrinkToFit: true,
+        datatype: 'json',
+        jsonReader: {
+            repeatitems: false,
+            root: "content",
+            page: function(result) {
+                //Total number of records
+                return result.number + 1;
             },
-            prmNames: {
-                page: "page.page",
-                rows: "page.size",
-                sort: "page.sort",
-                order: "page.sort.dir"
-            },
-            height: 'auto',
-            viewrecords: true,
-            rowList: [10, 20, 50, 100],
-            altRows: true,
-            loadError: function(xhr, status, error) {
+            total: "totalPages",
+            records: "totalElements"
+        },
+        prmNames: {
+            page: "page.page",
+            rows: "page.size",
+            sort: "page.sort",
+            order: "page.sort.dir"
+        },
+        height: 'auto',
+        viewrecords: true,
+        rowList: [10, 20, 50, 100],
+        altRows: true,
+        loadError: function(xhr, status, error) {
 
-            }
-        });
+        }
+    });
 
-        $.extend($.jgrid.edit, {
-            closeAfterEdit: true,
-            closeAfterAdd: true,
-            ajaxEditOptions: {contentType: "application/json"},
-            mtype: 'PUT',
-            serializeEditData: function(data) {
-                delete data.oper;
-                return JSON.stringify(data);
-            }
-        });
-        $.extend($.jgrid.del, {
-            mtype: 'DELETE',
-            serializeDelData: function() {
-                return "";
-            }
-        });
-        var delOptions = {
-            onclickSubmit: function(params, postdata) {
-                console.log(postdata);
-                params.url = URL + 'data/' + postdata;
-            }
-        };
+    $.extend($.jgrid.edit, {
+        closeAfterEdit: true,
+        closeAfterAdd: true,
+        ajaxEditOptions: {contentType: "application/json"},
+        mtype: 'PUT',
+        serializeEditData: function(data) {
+            delete data.oper;
+            return JSON.stringify(data);
+        }
+    });
+    $.extend($.jgrid.del, {
+        mtype: 'DELETE',
+        serializeDelData: function() {
+            return "";
+        }
+    });
+    var delOptions = {
+        onclickSubmit: function(params, postdata) {
+            console.log(postdata);
+            params.url = URL + 'data/' + entityName + '/' + postdata;
+        }
+    };
 
-        var colModelDynamic = [];
+    var colModelDynamic = [];
+    colModelDynamic.push({
+        name: 'name',
+        label: 'Name',
+        key: true,
+        index: 'name',
+        editable: true,
+        editoptions: {required: true}
+    });
+
+    $.each(contentData.attributes, function(index, attribute) {
         colModelDynamic.push({
-            name: 'name',
-            label: 'Name',
+            name: attribute.name,
+            label: attribute.description,
             key: true,
             index: 'name',
             editable: true,
             editoptions: {required: true}
         });
-
-        $.each(contentData.attributes, function(index, attribute) {
-            colModelDynamic.push({
-                name: attribute.name,
-                label: attribute.description,
-                key: true,
-                index: 'name',
-                editable: true,
-                editoptions: {required: true}
-            });
-        });
-        var options = {
-            url: URL + 'data/' + entityName,
-            editurl: URL,
-            colModel: colModelDynamic,
-            caption: entityName,
-            pager: '#pager',
-            height: 'auto',
-            ondblClickRow: function(id) {
-                //edit url
-            },
-            formatter: {idName: "name"}
-        };
-        $("#grid")
-                .jqGrid(options)
-                .navGrid('#pager',
-                        {search: false, addfunc: function() {
-                                window.location = 'dataListChange.html#' + "&entity=" + entityName;
-                            }, editfunc: function(data) {
-                                window.location = 'dataListChange.html' + '#id=' + data + "&entity=" + entityName;
-                            }}, //options
-                {}, // edit options
-                        {}, // add options 
-                        delOptions,
-                        {} // search options
-                );
+    });
+    var options = {
+        url: URL + 'data/' + entityName,
+        editurl: URL,
+        colModel: colModelDynamic,
+        caption: entityName,
+        pager: '#pager',
+        height: 'auto',
+        ondblClickRow: function(id) {
+            //edit url
+        },
+        formatter: {idName: "name"}
+    };
+    $("#grid")
+            .jqGrid(options)
+            .navGrid('#pager',
+                    {search: false, addfunc: function() {
+                            window.location = 'dataListChange.html#' + "&entity=" + entityName;
+                        }, editfunc: function(data) {
+                            window.location = 'dataListChange.html' + '#id=' + data + "&entity=" + entityName;
+                        }}, //options
+            {}, // edit options
+                    {}, // add options 
+                    delOptions,
+                    {} // search options
+            );
 }
 
 $(document).ready(function() {
@@ -110,8 +110,8 @@ $(document).ready(function() {
                 dataType: "json",
                 contentType: 'application/json',
                 success: function(data, textStatus, jqXHR)
-                {                    
-                    loadData(entityName,data.entity);
+                {
+                    loadData(entityName, data.entity);
 
                 },
                 error: function(jqXHR, textStatus, errorThrown)
