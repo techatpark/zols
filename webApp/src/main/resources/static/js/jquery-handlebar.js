4e6017472d6efb7636abcb33583a0b372100bceb
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2014-01-16 11:22:37 +0530

    Added Some Changes

--- webApp/src/main/resources/static/js/jquery-handlebar.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    var compiled = {};
-    $.fn.handlebars = function(templateName, data) {
-        var component = this;        
-        compileTemplate = Handlebars.compile($('#'+templateName).html());
-        component.append(compileTemplate(data));
-        
-    };
-})(jQuery);
