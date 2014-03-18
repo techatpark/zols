b742583285133e3ffa455106c1dd58f355b47212
praveen <praveen@sathish-VPCCW17FX>
2014-03-18 20:42:59 +0530

    Did some own changes...

--- datastore/src/main/java/com/zols/languagemanager/domain/DocumentStorage.java
+++ datastore/src/main/java/com/zols/languagemanager/domain/DocumentStorage.java
@@ -13,12 +13,64 @@ import org.springframework.data.annotation.Id;
  */
 public class DocumentStorage {
 
+   public static final String FILE_SYSTEM = "file";
+    public static final String FTP = "ftp";
+    public static final String CLASSPATH = "classpath";
+
     @Id
     private String name;
-
-    private String label;
-
     private String description;
+    private String type;
+    private String path;
+    private String host;
+    private String userName;
+    private String password;
+    private String rootFolder;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+
+
+    
+    
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRootFolder() {
+        return rootFolder;
+    }
+
+    public void setRootFolder(String rootFolder) {
+        this.rootFolder = rootFolder;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 
     public String getName() {
         return name;
@@ -28,19 +80,19 @@ public class DocumentStorage {
         this.name = name;
     }
 
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
