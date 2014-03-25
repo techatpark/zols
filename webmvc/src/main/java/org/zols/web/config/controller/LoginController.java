61a26c224a31b658fc15aa0990d0b78c0bf6a5e3
saravana <vsk62.gate@gmail.com>
2014-03-25 16:41:04 +0530

    Rename package to fix compilation issue

--- webmvc/src/main/java/org/zols/web/config/controller/LoginController.java
+++ webmvc/src/main/java/org/zols/web/config/controller/LoginController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.zols.datastore.web.controller;
+package org.zols.web.config.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
