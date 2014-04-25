1502117216bed072e4c24749799bce7d4e8b864c
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2014-04-25 10:45:27 +0530

    Functionality to see the build number from the UI #62

--- webmvc/src/main/java/org/zols/web/interceptor/PagePopulationInterceptor.java
+++ webmvc/src/main/java/org/zols/web/interceptor/PagePopulationInterceptor.java
@@ -5,12 +5,13 @@
  */
 package org.zols.web.interceptor;
 
-import org.zols.linkmanager.LinkManager;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.zols.linkmanager.LinkManager;
 
 /**
  *
@@ -21,12 +22,29 @@ public class PagePopulationInterceptor extends HandlerInterceptorAdapter {
     @Autowired
     private LinkManager linkManager;
 
+    private String appVersion;
+
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        if (modelAndView != null) {            
+        if (modelAndView != null) {
+            if(appVersion == null) {
+                appVersion = getVersion(request);
+            }
+            modelAndView.addObject("version", appVersion);
             modelAndView.addObject("links", linkManager.getApplicationLinks());
-            modelAndView.addObject("viewName",modelAndView.getViewName());
+            modelAndView.addObject("viewName", modelAndView.getViewName());
             modelAndView.setViewName("index");
         }
+    }
+
+    private String getVersion(HttpServletRequest request) {
+        String version = null;
+        Set<String> libPath = request.getServletContext().getResourcePaths("/WEB-INF/lib");
+        for (String libFile : libPath) {
+            if (libFile.contains("webmvc")) {
+                version = libFile.replace("/WEB-INF/lib/webmvc-", "").replaceAll(".jar", "");
+            }
+        }
+        return version;
     }
 }
