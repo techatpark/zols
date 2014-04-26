7c58596d7ab9423170254485f803e122b00c7bb8
AchyuthMJ <mj.achyuth@gmail.com>
2014-04-26 12:56:00 +0530

    Document Manager documentation

--- documentmanager/src/main/java/org/zols/documentmanager/DocumentStorageManager.java
+++ documentmanager/src/main/java/org/zols/documentmanager/DocumentStorageManager.java
@@ -22,22 +22,45 @@ public class DocumentStorageManager {
     @Autowired
     private DataStore dataStore;
 
+    /**
+     * Add a new document storage
+     * @param documentStorage to be added
+     * @return an instance of the new document storage
+     */
     public DocumentStorage add(DocumentStorage documentStorage) {
         return dataStore.create(documentStorage, DocumentStorage.class);
     }
 
+    /**
+     * Update a document storage.
+     * @param documentStorage to be updated.
+     */
     public void update(DocumentStorage documentStorage) {
         dataStore.update(documentStorage, DocumentStorage.class);
     }
 
+    /**
+     * Delete a document storage from the system
+     * @param documentStorageName name of the document storage to be deleted.
+     */
     public void delete(String documentStorageName) {
         dataStore.delete(documentStorageName, DocumentStorage.class);
     }
 
+    /**
+     * Get a document storage by its name
+     * @param documentStorageName name of the document storage.
+     * @return the document storage.
+     */
     public DocumentStorage get(String documentStorageName) {
         return dataStore.read(documentStorageName, DocumentStorage.class);
     }
 
+    /**
+     * List the document storages in the system
+     * @param page
+     * @return 
+     */
     public Page<DocumentStorage> list(Pageable page) {
         return dataStore.list(page, DocumentStorage.class);
     }
