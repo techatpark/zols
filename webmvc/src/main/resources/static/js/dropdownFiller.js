79b4a757d9aa04dfc27045bbc858e3027ea0bd33
praveen <praveen@sathish-VPCCW17FX>
2014-05-01 08:27:45 +0530

    Fix: Duplicate Values

--- webmvc/src/main/resources/static/js/dropdownFiller.js
+++ webmvc/src/main/resources/static/js/dropdownFiller.js
@@ -17,24 +17,30 @@
     };
 
     $.fn.loadData = function(selectBox) {
-        var contextpath = document.URL.substring(document.URL.indexOf('/',document.URL.indexOf('//')+2));
-        contextpath = contextpath.substring(0,contextpath.indexOf('/',2));
-        $.ajax({
-            url: contextpath + '/master/' + selectBox.attr('data-master'),
-            success: function(result) {
-                $.each(result, function() {
-                    selectBox.append($("<option />").val(this.name).text(this.label));
-                });
-                if (selectBox.attr('value')) {
-                    selectBox.val(selectBox.attr('value'));
+
+        var contextpath = document.URL.substring(document.URL.indexOf('/', document.URL.indexOf('//') + 2));
+        contextpath = contextpath.substring(0, contextpath.indexOf('/', 2));
+        // Load From Data Store
+        if (selectBox.is(':empty')) {
+            $.ajax({
+                url: contextpath + '/master/' + selectBox.attr('data-master'),
+                async: false,
+                success: function(result) {
+                    $.each(result, function() {
+                        selectBox.append($("<option />").val(this.name).text(this.label));
+                    });
+                    if (selectBox.attr('value')) {
+                        selectBox.val(selectBox.attr('value'));
+                    }
                 }
-            }
-        });
+            });
+        }
 
         // Load From Data Store
         if (selectBox.is(':empty')) {
             $.ajax({
                 url: contextpath + '/master/all/' + selectBox.attr('data-master'),
+                async: false,
                 success: function(result) {
                     $.each(result[selectBox.attr('data-master')], function() {
                         selectBox.append($("<option />").val(this.name).text(this.name));
@@ -44,7 +50,7 @@
                     }
                 },
                 error: function(error) {
-                    
+
                 }
             });
         }
