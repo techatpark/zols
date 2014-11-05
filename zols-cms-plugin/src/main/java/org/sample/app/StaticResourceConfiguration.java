a7be6c04b64d3d47aefabb94903171d41aae8db8
sathish <sathish@Sathish-PC>
2014-11-05 15:12:54 +0530

    dandanakka static working

--- zols-cms-plugin/src/main/java/org/sample/app/StaticResourceConfiguration.java
+++ zols-cms-plugin/src/main/java/org/sample/app/StaticResourceConfiguration.java
@@ -14,6 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/custom/**").addResourceLocations("classpath:static2/","classpath:templates/");
+        registry.addResourceHandler("/custom/**").addResourceLocations("classpath:static2/","file:/home/sathish/MAG/");
     }
 }
