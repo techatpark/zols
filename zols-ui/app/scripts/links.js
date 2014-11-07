224aead901e2d719505e55bb4de70f14f8e5af5c
saravana <vsk62.gate@gmail.com>
2014-11-07 12:29:32 +0530

    Fix creating new categories

--- zols-ui/app/scripts/links.js
+++ zols-ui/app/scripts/links.js
@@ -3,6 +3,10 @@
 (function($) {
     var base_url = 'http://localhost:8080/api';
 
+    $.ajaxSetup({
+      contentType: 'application/json',
+    })
+
     var template;
 
     var selectedCategory;
@@ -20,10 +24,15 @@
     };
 
     $.fn.saveCategory = function() {
-        $.post(base_url + '/link_categories', selectedCategory)
-                .done(function(data) {
-                    alert("Data Loaded: " + data);
-                });
+      console.log(selectedCategory);
+      $.ajax({
+        method: 'POST',
+        url: base_url + '/link_categories',
+        dataType : 'json',
+        data:  JSON.stringify(selectedCategory)
+      }).done(function(data) {
+        alert("Data Loaded: " + data);
+      });
     };
     $.fn.editFirstCategory = function() {
         $.get(base_url + '/link_categories').done(function(data) {
