2f6ad81c582b2d36984a624e51e081d153d081a3
sathish <sathish@Sathish-PC>
2014-11-07 11:31:34 +0530

    Create New does not work. Why ?

--- zols-ui/app/scripts/links.js
+++ zols-ui/app/scripts/links.js
@@ -1,37 +1,47 @@
 'use strict';
 
-(function($) {    
+(function($) {
     var base_url = 'http://localhost:8080/api';
-    var currentData;
+
     var template;
+
+    var selectedCategory;
+
+    $('#createCategory').click(function() {
+        $.fn.createCategory();
+    });
 
     $.fn.listFirstCategory = function() {
         $.get(base_url + '/link_categories').done(function(data) {
             var template = $.templates("#viewLink");
-            var htmlOutput = template.render(data[0]);
-            $("#result").html(htmlOutput);
+            var htmlOutput = template.render({category: data});
+            $("#categories").prepend(htmlOutput);
         });
     };
 
-    $.fn.changeHandler = function(ev, eventArgs) {
-        console.log(currentData);
-        var message = "The new '" + eventArgs.path + "' is '"
-                + eventArgs.value + "'.";
+    $.fn.saveCategory = function() {
+        $.post(base_url + '/link_categories', selectedCategory)
+                .done(function(data) {
+                    alert("Data Loaded: " + data);
+                });
     };
     $.fn.editFirstCategory = function() {
-        $.get(base_url+'/link_categories').done(function(data) {
+        $.get(base_url + '/link_categories').done(function(data) {
             template = $.templates("#editLink");
-            currentData = data[0];
-            console.log(currentData);
-            template.link('#result', currentData);
-            $(currentData).on("propertyChange", $.fn.changeHandler);
+            selectedCategory = data[0];
+            console.log(selectedCategory);
+            template.link('#categories', selectedCategory);
+            $(selectedCategory).on("propertyChange", $.fn.changeHandler);
         });
     };
 
-    $.fn.editCategory = function() {
-        $.fn.editFirstCategory();
+    $.fn.createCategory = function() {
+        selectedCategory = {};
+        template = $.templates("#editLink");
+        template.link('#result', selectedCategory);
     };
-    
+
+
     $.fn.listFirstCategory();
 
 
