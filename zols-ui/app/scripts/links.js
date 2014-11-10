6e8b0528ecd021d41c74251c88c5fa20a19affe5
sathish <sathish@Sathish-PC>
2014-11-10 12:46:42 +0530

    Fixes almost done

--- zols-ui/app/scripts/links.js
+++ zols-ui/app/scripts/links.js
@@ -4,14 +4,15 @@
     var base_url = 'http://localhost:8080/api';
 
     $.ajaxSetup({
-        contentType: 'application/json',
+        contentType: 'application/json'
     });
 
     var template;
     var selectedCategory;
-    var selectedParantLinkName;
+    var parentLinks = [];
     var selectedLink;
     var currentLinks;
+
     var confirmationPromise;
 
     $('#edit_selected').on('click', function() {
@@ -46,7 +47,7 @@
             $('#categories .catName').on('click', function() {
                 $.fn.setSelectedCategory($.view(this).data);
             });
-            selectedParantLinkName = null;
+            parentLinks = [];
         });
     };
 
@@ -59,42 +60,67 @@
         });
 
     };
-    $.fn.listCategoryLinks = function(listofLinks) {        
-
+    $.fn.listCategoryLinks = function(listofLinks) {
         currentLinks = {link: listofLinks};
-
         var template = $.templates("#listLink");
         template.link('#result', currentLinks);
-
         $('#addMoreLinkBtn').on('click', function() {
             $.fn.createLink();
         });
 
         $('#result li a').on('click', function() {
-            $.fn.setSelectedParentLink($.view(this).data);
+            $.fn.addParentLink($.view(this).data);
         });
 
     };
 
-    $.fn.setSelectedParentLink = function(ParentLink) {
-        selectedParantLinkName = ParentLink;
-        $.get(base_url + '/links/childen_of/' + selectedParantLinkName.name).done(function(data) {
+    $.fn.addParentLink = function(ParentLinkData) {
+        parentLinks.push(ParentLinkData);
+        $.fn.listChildren(ParentLinkData);
+    };
+
+    $.fn.listChildren = function(ParentLinkData) {
+        $.get(base_url + '/links/childen_of/' + ParentLinkData.name).done(function(data) {
             $.fn.listCategoryLinks(data);
         });
-        $('#linksBreadcrumb').append('<li><a href="#">'+selectedParantLinkName.label+'</a></li>');
-        
+        template = $.templates("#breadcrumb");
+        template.link('#linksBreadcrumb', {parentLink: parentLinks});
+        $('#linksBreadcrumb a').on('click', function() {
+            var selectedLinkIndex = $(this).parent().index();
+            if (selectedLinkIndex === 0) {
+
+            } else {
+                var parentLinkData = parentLinks[selectedLinkIndex - 1];
+                parentLinks = parentLinks.slice(0, selectedLinkIndex);
+                $.fn.listChildren(parentLinkData);
+            }
+
+        });
     };
 
     $.fn.saveCategory = function() {
-        console.log(selectedCategory);
-        $.ajax({
-            method: 'POST',
-            url: base_url + '/link_categories',
-            dataType: 'json',
-            data: JSON.stringify(selectedCategory)
-        }).done(function(data) {
-            alert("Data Loaded: " + data);
-        });
+        if (selectedCategory.isEdit) {
+            delete selectedCategory.isEdit;
+            $.ajax({
+                method: 'PUT',
+                url: base_url + '/link_categories/' + selectedCategory.name,
+                dataType: 'json',
+                data: JSON.stringify(selectedCategory)
+            }).done(function(data) {
+                $.fn.listCategories();
+            });
+        }
+        else {
+            $.ajax({
+                method: 'POST',
+                url: base_url + '/link_categories',
+                dataType: 'json',
+                data: JSON.stringify(selectedCategory)
+            }).done(function(data) {
+                $.fn.listCategories();
+            });
+        }
+
     };
     $.fn.deleteSelectedCategory = function() {
         $.ajax({
@@ -106,8 +132,11 @@
         });
     };
     $.fn.editSelectedCategory = function() {
-        template = $.templates("#catetoryForm");
-        template.link('#result', selectedCategory);
+        if (selectedCategory) {
+            template = $.templates("#catetoryForm");
+            selectedCategory.isEdit = true;
+            template.link('#result', selectedCategory);
+        }
     };
 
     $.fn.createCategory = function() {
@@ -122,17 +151,28 @@
         template.link('#result', selectedLink);
     };
 
+    $.fn.refreshList = function() {
+        if (parentLinks.length !== 0) {
+            $fn.listChildren(parentLinks[parentLinks.length - 1]);
+        }
+        else {
+            $.fn.listCategories();
+        }
+    };
+
     $.fn.saveLink = function() {
         console.log(selectedLink);
         selectedLink.categoryName = selectedCategory.name;
-        selectedLink.parentLinkName = selectedParantLinkName;
+        if (parentLinks.length !== 0) {
+            selectedLink.parentLinkName = parentLinks[parentLinks.length - 1].name;
+        }
         $.ajax({
             method: 'POST',
             url: base_url + '/links',
             dataType: 'json',
             data: JSON.stringify(selectedLink)
         }).done(function(data) {
-            alert("Data Loaded: " + data);
+            $.fn.refreshList();
         });
     };
 
