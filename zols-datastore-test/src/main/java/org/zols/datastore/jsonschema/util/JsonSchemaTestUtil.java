3541a0baade2a97d802031b239939b671c2953a0
Sathish Kumar <sathish@ooyala.com>
2017-08-18 17:26:46 +0530

    Created Separate Datastore Tests 2

--- zols-datastore-test/src/main/java/org/zols/datastore/jsonschema/util/JsonSchemaTestUtil.java
+++ zols-datastore-test/src/main/java/org/zols/datastore/jsonschema/util/JsonSchemaTestUtil.java
@@ -5,12 +5,15 @@
  */
 package org.zols.datastore.jsonschema.util;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import static java.lang.ClassLoader.getSystemResource;
 import java.net.URISyntaxException;
 import static java.nio.file.Files.readAllBytes;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static org.zols.datastore.util.JsonUtil.asMap;
@@ -20,7 +23,7 @@ import static org.zols.datastore.util.JsonUtil.asMap;
  * @author WZ07
  */
 public class JsonSchemaTestUtil {
-    
+
     public static Map<String, Object> sampleJsonSchema(String dataName) {
         return asMap(sampleJsonSchemaText(dataName));
     }
@@ -28,9 +31,8 @@ public class JsonSchemaTestUtil {
     public static String sampleJsonSchemaText(String schamaName) {
         String schemaContent = null;
         try {
-            schemaContent = new String(readAllBytes(Paths
-                    .get(getSystemResource("org/zols/datastore/jsonschema/schema/" + schamaName + ".json").toURI())));
-        } catch (IOException | URISyntaxException ex) {
+            schemaContent = getFile("org/zols/datastore/jsonschema/schema/" + schamaName + ".json");
+        } catch (IOException ex) {
             Logger.getLogger(JsonSchemaTestUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
         return schemaContent;
@@ -39,9 +41,8 @@ public class JsonSchemaTestUtil {
     public static String sampleJsonText(String dataName) {
         String schemaContent = null;
         try {
-            schemaContent = new String(readAllBytes(Paths
-                    .get(getSystemResource("org/zols/datastore/jsonschema/jsondata/" + dataName + ".json").toURI())));
-        } catch (IOException | URISyntaxException ex) {
+            schemaContent = getFile("org/zols/datastore/jsonschema/jsondata/" + dataName + ".json");
+        } catch (IOException ex) {
             Logger.getLogger(JsonSchemaTestUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
         return schemaContent;
@@ -49,5 +50,24 @@ public class JsonSchemaTestUtil {
 
     public static Map<String, Object> sampleJson(String dataName) {
         return asMap(sampleJsonText(dataName));
+    }
+
+    private static String getFile(String fileName) throws IOException {
+
+        StringBuilder result = new StringBuilder();
+
+        //Get file from resources folder
+        ClassLoader classLoader = JsonSchemaTestUtil.class.getClassLoader();
+        InputStream fileStream = classLoader.getResourceAsStream(fileName);
+
+        try (Scanner scanner = new Scanner(fileStream)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+        }
+
+        return result.toString();
+
     }
 }
