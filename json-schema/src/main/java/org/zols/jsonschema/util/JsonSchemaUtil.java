fb14692df3bff9263d01745f9d6b3594da287324
sathishk <sathishkumar.thiyagarajan@gmail.com>
2017-10-04 12:38:31 +0530

    Added validate Schema

--- json-schema/src/main/java/org/zols/jsonschema/util/JsonSchemaUtil.java
+++ json-schema/src/main/java/org/zols/jsonschema/util/JsonSchemaUtil.java
@@ -5,17 +5,23 @@
  */
 package org.zols.jsonschema.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.jsonSchema.factories.SchemaFactoryWrapper;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.ConstraintViolation;
 import org.zols.jsonschema.JsonSchema;
 import org.zols.jsonschema.everit.EveritJsonSchema;
 
@@ -24,7 +30,40 @@ import org.zols.jsonschema.everit.EveritJsonSchema;
  * @author sathish
  */
 public class JsonSchemaUtil {
-    
+
+    private static JsonSchema _JSONSCHEMA_FOR_SCHEMA = null;
+
+    private static String getContentFromClasspath(String resourcePath) {
+        InputStream inputStream = JsonSchemaUtil.class.getResourceAsStream(resourcePath);
+        String theString;
+        try (java.util.Scanner scanner = new java.util.Scanner(inputStream, "UTF-8").useDelimiter("\\A")) {
+            theString = scanner.hasNext() ? scanner.next() : "";
+        }
+        return theString;
+    }
+
+    public static JsonSchema jsonSchemaForSchema() {
+        if (_JSONSCHEMA_FOR_SCHEMA == null) {
+
+            Map<String, Object> schema;
+            try {
+                schema = new ObjectMapper().readValue(getContentFromClasspath("/org/zols/jsonschema/schema.json"),
+                        new TypeReference<HashMap<String, Object>>() {
+                });
+                _JSONSCHEMA_FOR_SCHEMA = new EveritJsonSchema(schema, null);
+            } catch (IOException ex) {
+                Logger.getLogger(JsonSchemaUtil.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+        return _JSONSCHEMA_FOR_SCHEMA;
+    }
+
+    public static Set<ConstraintViolation> validateSchema(Map<String, Object> jsonData) {
+        return jsonSchemaForSchema().validate(jsonData);
+    }
+
     public static JsonSchema getJsonSchema(Class clazz) {
         ObjectMapper m = new ObjectMapper();
         SchemaFactoryWrapper visitor = new SchemaFactoryWrapper();
@@ -49,10 +88,10 @@ public class JsonSchemaUtil {
                 ids.add(field.getName());
             }
         }
-        
-        if(!ids.isEmpty()) {
+
+        if (!ids.isEmpty()) {
             jsonSchemaAsMap.put("ids", ids);
         }
-        return new EveritJsonSchema(jsonSchemaAsMap,null);
+        return new EveritJsonSchema(jsonSchemaAsMap, null);
     }
 }
---
+++ json-schema/src/test/java/org/zols/jsonschema/util/JsonSchemaUtilTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.zols.jsonschema.util;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import static org.zols.jsonschema.util.TestUtil.getTestSchema;
+
+/**
+ *
+ * @author sathish
+ */
+public class JsonSchemaUtilTest {
+    
+
+    /**
+     * Test of validateSchema method, of class JsonSchemaUtil.
+     */
+    @Test
+    public void testValidateSchema() {
+
+        assertTrue("Checking valid schema", JsonSchemaUtil.validateSchema(getTestSchema("product"))==null);
+        assertTrue("Checking invalid schema", !JsonSchemaUtil.validateSchema(getTestSchema("product_invalid")).isEmpty());
+    }
+
+    
+    
+}
