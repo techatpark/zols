10908abc7ba42ed1ac1ee30d78c1172375236289
sathishk <sathishkumar.thiyagarajan@gmail.com>
2017-10-27 19:52:03 +0530

    Removed unused files

--- zols-datastore/src/main/resources/org/zols/datastore/jsonschema/validator.js
+++
@@ -1,7 +0,0 @@
-function validateJsonSchema(schema, data) {
-    var validate = jsen(schema);
-    if (!validate(data)) {
-        return validate.errors;
-    }
-    return null;         // true
-}
