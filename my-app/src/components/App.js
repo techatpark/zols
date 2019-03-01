7312bddc20023dbe8799b5fbba71eecd75e42ee1
sathishk <sathishkumar.thiyagarajan@gmail.com>
2019-03-01 16:22:00 +0530

    json formed

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import Header from './Header'
-import Main from './Main'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Home";
+// import Data from "./Data";
+import Data from "./datainstance/DataList";
 
 const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
+  <div class="container">
+
+
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Search Studio</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
+              <li><a href="../navbar-static-top/">Static top</a></li>
+              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      
+      <div class="jumbotron">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/data/:schemaId" component={Data} />
+      </Switch>
+      </div>
+
+    </div>
 )
 
 export default App
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 render(
   <BrowserRouter>
