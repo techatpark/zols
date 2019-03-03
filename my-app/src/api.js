ad96830089a2e1d9f86657d85599fa16582f9911
sathishk <sathishkumar.thiyagarajan@gmail.com>
2019-03-03 09:19:02 +0530

    imhertoance support

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -6,6 +6,16 @@ const api = axios.create({
   baseURL: BASE_URL
 });
 const APIModel = {
+  getPatchSchema: schema => {
+    const patched_schema = Object.assign({}, schema);
+    var current_schema = patched_schema;
+    while(current_schema["$ref"] != undefined) {
+      current_schema = patched_schema.definitions[current_schema["$ref"].split('#/definitions/')[1]];
+      Object.assign(patched_schema.properties, current_schema.properties);
+      console.log(current_schema.title);
+    }
+    return patched_schema;
+  },
   get: route => api.get(route),
   post: (route, data) => api.post(route, data),
   put: (route, data) => api.put(route, data),
--- my-app/src/components/datainstance/Data.js
+++ my-app/src/components/datainstance/Data.js
@@ -4,7 +4,7 @@ import Api from "../../api";
 
 export default class Data extends Component {
 
-  
+
   state = {
     schema: {},
     data: {}
@@ -12,11 +12,20 @@ export default class Data extends Component {
 
   componentDidMount = async () => {
     const schemaId = this.props.match.params.schemaId;
-    const locpath = this.props.location.pathname.split('/data/asset/')[1];
-    const axis_schema = await Api.get(`/schema/${schemaId}`);
-    console.log(locpath);
-    const axis_data = await Api.get(`/data/${schemaId}/${locpath}`);
-    this.setState({ schema: axis_schema.data, data:  axis_data.data});
+    const locpath = this.props.location.pathname.split('/data/')[1].split('/')[1];
+    const axis_schema = await Api.get(`/schema/${schemaId}?enlarged`);
+
+    const patched_schema = Api.getPatchSchema(axis_schema.data);
+
+    console.log(patched_schema);
+
+    if(locpath != "_addNew") {
+      const axis_data = await Api.get(`/data/${schemaId}/${locpath}`);
+      this.setState({ schema: patched_schema, data:  axis_data.data});
+    }else {
+      this.setState({ schema: patched_schema});
+    }
+
   };
   render() {
     return (
