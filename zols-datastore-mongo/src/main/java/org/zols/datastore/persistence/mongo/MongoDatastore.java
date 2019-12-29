727c156f5ae42a39682501d9b8cb19f13f884fb1
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2019-12-29 13:18:45 +0530

    test

--- zols-datastore-mongo/src/main/java/org/zols/datastore/persistence/mongo/MongoDatastore.java
+++ zols-datastore-mongo/src/main/java/org/zols/datastore/persistence/mongo/MongoDatastore.java
@@ -6,11 +6,13 @@
 package org.zols.datastore.persistence.mongo;
 
 import com.mongodb.client.MongoDatabase;
+import cz.jirutka.rsql.parser.ast.Node;
+import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
+import org.zols.datastore.DataStore;
 import org.zols.datastore.persistence.DataStorePersistence;
 import org.zols.datastore.query.Page;
-import org.zols.datastore.query.Query;
 import org.zols.datatore.exception.DataStoreException;
 import org.zols.jsonschema.JsonSchema;
 
@@ -27,47 +29,63 @@ public class MongoDatastore implements DataStorePersistence {
     }
 
     @Override
+    public void onIntialize(DataStore dataStore) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onNewSchema(JsonSchema jsonSchema) throws DataStoreException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onUpdateSchema(JsonSchema oldSchema, JsonSchema newSchema) throws DataStoreException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onDeleteSchema(JsonSchema jsonSchema) throws DataStoreException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
     public Map<String, Object> create(JsonSchema jsonSchema, Map<String, Object> jsonData) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Map<String, Object> read(JsonSchema jsonSchema, String idValue) throws DataStoreException {
+    public Map<String, Object> read(JsonSchema jsonSchema, AbstractMap.SimpleEntry<String, Object>... idValues) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean delete(JsonSchema jsonSchema, String idValue) throws DataStoreException {
+    public boolean delete(JsonSchema jsonSchema, AbstractMap.SimpleEntry<String, Object>... idValues) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean delete(JsonSchema jsonSchema, Query query) throws DataStoreException {
+    public boolean delete(JsonSchema jsonSchema, Node queryNode) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean update(JsonSchema jsonSchema, String idValue, Map<String, Object> jsonData) throws DataStoreException {
+    public boolean update(JsonSchema jsonSchema, Map<String, Object> jsonData, AbstractMap.SimpleEntry<String, Object>... idValues) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean updatePartially(JsonSchema jsonSchema, String idValue, Map<String, Object> jsonData) throws DataStoreException {
+    public boolean updatePartially(JsonSchema jsonSchema, Map<String, Object> jsonData, AbstractMap.SimpleEntry<String, Object>... idValues) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<Map<String, Object>> list(JsonSchema jsonSchema, Query query) throws DataStoreException {
+    public List<Map<String, Object>> list(JsonSchema jsonSchema, Node queryNode) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Page<Map<String, Object>> list(JsonSchema jsonSchema, Query query, Integer pageNumber, Integer pageSize) throws DataStoreException {
+    public Page<Map<String, Object>> list(JsonSchema jsonSchema, Node queryNode, Integer pageNumber, Integer pageSize) throws DataStoreException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public void drop() throws DataStoreException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
