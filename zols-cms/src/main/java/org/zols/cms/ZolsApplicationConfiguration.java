de782b98876425b799066a03668a0f528160ceee
sathishk <sathishkumar.thiyagarajan@gmail.com>
2020-09-17 13:31:39 +0530

    ES Configurable now

--- zols-cms/src/main/java/org/zols/cms/ZolsApplicationConfiguration.java
+++ zols-cms/src/main/java/org/zols/cms/ZolsApplicationConfiguration.java
@@ -8,6 +8,7 @@ package org.zols.cms;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,14 +20,13 @@ public class ZolsApplicationConfiguration {
     
     @Value("${spring.application.name}")
     private String indexName;
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
         
     @Bean
     public DataStore dataStore() {
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")));
-        return new DataStore(new ElasticSearchDataStorePersistence(indexName,client));
+        return new DataStore(new ElasticSearchDataStorePersistence(indexName,restHighLevelClient));
     }
 
 }
