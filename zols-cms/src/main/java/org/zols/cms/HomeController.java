9eaf528b899d20d8199f83d21c89fce760981431
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-12-31 13:50:57 +0530

    Removed Unused Code

---
+++ zols-cms/src/main/java/org/zols/cms/HomeController.java
@@ -0,0 +1,14 @@
+package org.zols.cms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "forward:/index.html";
+    }
+
+}
