30d7cbd69e2709efc4c0327943df7be1fa133111
Manikanta445 <manikantayarramsetti445@gmail.com>
2022-01-04 16:28:21 +0530

    click event

--- zols-ui/src/js/pages/user.js
+++ zols-ui/src/js/pages/user.js
@@ -5,3 +5,8 @@ class UserScreen {
 	}
 }
 new UserScreen();
+
+const plusBtn = document.getElementsByClassName("fas fa-plus")[0];
+plusBtn.addEventListener("click", function () {
+	console.log("clicked plus button to add users");
+});
