d88ee0647ab037dcc775d52839a034cc5b81b1bc
sathishk <sathishkumar.thiyagarajan@gmail.com>
2022-01-04 16:53:34 +0530

    Completed

--- zols-ui/src/js/pages/user.js
+++ zols-ui/src/js/pages/user.js
@@ -2,11 +2,21 @@
 class UserScreen {
 	constructor() {
 		console.log("I am code behind for users");
+		this.setup();
+	}
+
+	setup() {
+		// Bind Call to actions
+
+		document.querySelector("i.fa-plus").addEventListener("click", () => {
+			console.log("I am call to action method for add user");
+		});
+
+		document.querySelectorAll("i.fa-pencil-alt").forEach((element) => {
+			element.addEventListener("click", () => {
+				console.log("I am call to action method for edit user");
+			});
+		});
 	}
 }
 new UserScreen();
-
-const plusBtn = document.getElementsByClassName("fas fa-plus")[0];
-plusBtn.addEventListener("click", function () {
-	console.log("clicked plus button to add users");
-});
